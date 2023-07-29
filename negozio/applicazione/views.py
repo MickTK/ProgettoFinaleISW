e9200461d27e6d42f533ba921eab7216d1da5510
Michele <63038410+MickTK@users.noreply.github.com>
2023-07-29 15:50:56 +0200

    Vista di prova

--- negozio/applicazione/views.py
+++ negozio/applicazione/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
-# Create your views here.
+def login(request):
+  template = loader.get_template("login.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
