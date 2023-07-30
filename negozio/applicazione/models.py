7b0c0c714e071f27a6a04497ed8deb4ce2570ef3
Michele <63038410+MickTK@users.noreply.github.com>
2023-07-30 13:19:35 +0200

    Test unitari (modelli)
    - Aggiunti dei test unitari per i modelli
    - Modificata la struttura di alcuni modelli e propagate le modifiche nel diagramma delle classi

--- negozio/applicazione/models.py
+++ negozio/applicazione/models.py
@@ -26,56 +26,60 @@ class Stock(models.Model):
 
   # Aggiunge un nuovo prodotto al negozio
   def aggiungi_nuovo_prodotto(self,nome,tipologia,descrizione,prezzo,quantita=1):
-    Prodotto(
+    prodotto = Prodotto(
       nome = nome,
       tipologia = tipologia,
       descrizione = descrizione,
       prezzo = prezzo,
       quantita = quantita,
       stock = self
-    ).save()
+    )
+    prodotto.save()
+    return prodotto
 
   # Rimuove completamente un prodotto dal negozio
   def rimuovi_prodotto(self,prodotto):
     if prodotto.stock.id == self.id:
       prodotto.delete()
 
-  def __eq__(self, other):
-    return self.id == other.id
-
 class Carrello(models.Model):
+
+  # Aggiunge un'unità di prodotto al carrello
+  def aggiungi_prodotto(self, prodotto, quantita=1):
+    prodotto_carrello = ProdottoCarrello.objects.filter(carrello=self,prodotto=prodotto)
+    if quantita < 1:
+      quantita = 1
+    # Incrementa la quantità
+    if prodotto_carrello.count() > 0:
+      prodotto_carrello.quantita += quantita
+      prodotto_carrello.save()
+    # Aggiunge il prodotto al carrello
+    else:
+      prodotto_carrello = ProdottoCarrello.objects.create(
+        quantita=quantita,
+        carrello=self,
+        prodotto=prodotto
+      )
+    return prodotto_carrello
+
+  # Rimuove un'unità di prodotto al carrello
+  def rimuovi_prodotto(self, prodotto, quantita=1):
+    prod = ProdottoCarrello.objects.get(carrello=self,prodotto=prodotto)
+    if quantita < 1:
+      quantita = 1
+    # Decrementa la quantità
+    if prod != None:
+      prod.quantita -= quantita
+      # Se viene rimosso l'ultima unità di prodotto del carrello
+      prod.delete() if prod.quantita < 1 else prod.save()
 
   # Il prezzo totale dei prodotti nel carrello
   def totale(self):
-    prodotti = ProdottoCarrello.objects.get(carrello=self)
+    prodotti = ProdottoCarrello.objects.filter(carrello=self)
     totale = 0
     for prodotto in prodotti:
       totale += prodotto.prodotto.prezzo * prodotto.quantita
     return totale
-  
-  # Aggiunge un'unità di prodotto al carrello
-  def aggiungi_prodotto(self,prodotto):
-    prod = ProdottoCarrello.objects.get(carrello=self,prodotto=prodotto)
-    # Incrementa la quantità
-    if prod != None:
-      prod.quantita += 1
-      prod.save()
-    # Aggiunge il prodotto al carrello
-    else:
-      ProdottoCarrello(
-        quantita=1,
-        carrello=self,
-        prodotto=prodotto
-      ).save()
-
-  # Rimuove un'unità di prodotto al carrello
-  def rimuovi_prodotto(self,prodotto):
-    prod = ProdottoCarrello.objects.get(carrello=self,prodotto=prodotto)
-    # Decrementa la quantità
-    if prod != None:
-      prod.quantita -= 1
-      # Se viene rimosso l'ultima unità di prodotto del carrello
-      prod.delete() if prod.quantita < 1 else prod.save()
 
 #================================================
 # Prodotto
@@ -88,7 +92,7 @@ class Prodotto(models.Model):
   descrizione = models.CharField(max_length=500)
   prezzo = models.FloatField()
   quantita = models.IntegerField()
-  stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+  stock = models.ForeignKey(Stock,on_delete=models.CASCADE,related_name='prodotti')
 
   # Modifica le informazioni del Prodotto
   # @params (string)nome, (string)tipologia, (string)descrizione, (float)prezzo, (integer)quantita
@@ -105,30 +109,31 @@ class Prodotto(models.Model):
       self.quantita = kwargs["quantita"]
     self.save()
 
-  def __eq__(self, other):
-    return (self.nome, self.tipologia, self.descrizione, self.prezzo, self.quantita, self.stock) == (other.nome, other.tipologia, other.descrizione, other.prezzo, other.quantita, other.stock)
-
 # Prodotto presente nel Carrello del Cliente (indica in che quantità è presente)
 class ProdottoCarrello(models.Model):
   quantita = models.IntegerField()
-  carrello = models.ForeignKey(Carrello,on_delete=models.CASCADE)
+  carrello = models.ForeignKey(Carrello,on_delete=models.CASCADE,related_name='prodotti')
   prodotto = models.ForeignKey(Prodotto,on_delete=models.CASCADE)
 
   # Modifica la quantità di un Prodotto all'interno del Carrello
+  # @param quantita: un valore intero che verrà sommato alla quantita attuale di prodotto
   def modifica_quantita(self, quantita):
-    self.quantita = quantita
-    self.delete() if quantita < 1 else self.save()
+    self.quantita += quantita
+    self.delete() if self.quantita < 1 else self.save()
 
   # Rimuove il ProdottoCarrello corrente e ne crea una copia come ProdottoVenduto
   def acquista(self):
-    ProdottoVenduto(
+    ProdottoVenduto.objects.create(
       nome = self.prodotto.nome,
       tipologia = self.prodotto.tipologia,
       descrizione = self.prodotto.descrizione,
       prezzo = self.prodotto.prezzo,
-      quantita = self.quantita
-    ).save()
-    Stock.objects.get(name="negozio").rimuovi_prodotto(self.prodotto, self.quantita)
+      quantita = self.quantita,
+      stock = self.prodotto.stock
+    )
+    self.prodotto.quantita -= self.quantita
+    if self.prodotto.quantita < 1:
+      self.prodotto.stock.rimuovi_prodotto(self.prodotto)
     self.delete()
 
 # Informazioni relative ad un prodotto venduto (serve per il resoconto)
--- negozio/tests/unitari/test_models.py
+++ negozio/tests/unitari/test_models.py
@@ -1,46 +1,130 @@
 from django.test import TestCase
 from applicazione.models import *
 
-class StockTestCase(TestCase):
+class MasterTestCase(TestCase):
   def setUp(self):
-    self.stock = Stock.objects.create(nome = "negozio")
-  def test_aggiungi_nuovo_prodotto(self):
+    self.stock = Stock.objects.create(nome = "Negozio")
     self.stock.aggiungi_nuovo_prodotto(
-      "Lavatrice",
-      "Elettrodomestici",
-      "Una bellissima lavatrice",
-      200,
-      100
+      "Motorino",
+      "Veicolo",
+      "Motorino elettrico ecologico.",
+      499.99,
+      25
     )
-    prodotto = Prodotto.objects.get(nome = "Lavatrice", prezzo = 200)
-    self.assertEqual(self.stock.id,prodotto.stock.id)
-  def test_rimuovi_prodotto(self):
-    self.stock.aggiungi_nuovo_prodotto(
-      "Lavatrice",
-      "Elettrodomestici",
-      "Una bellissima lavatrice",
-      200,
-      100
+    self.cliente = Cliente.objects.create(
+      username = "user",
+      password = "pass"
     )
-    prodotto = Prodotto.objects.get(nome = "Lavatrice", prezzo = 200)
-    #self.stock.rimuovi_prodotto(prodotto)
-    prodotto = Prodotto.objects.get(stock = self.stock)
-    #self.assertEqual(prodotto, None)
+    self.carrello = self.cliente = Carrello.objects.create()
 
-class ProdottoTestCase(TestCase):
+#==============================================================================
+# Negozio
+#==============================================================================
+
+class StockTestCase(MasterTestCase):
+
   def setUp(self):
-    Stock.objects.create(nome = "negozio")
-    self.prodotto = Prodotto.objects.create(
-      nome = "Smart TV",
-      tipologia = "Televisore",
-      descrizione = "Televisore con riconoscimento vocale.",
-      prezzo = 1499.99,
-      quantita = 10,
-      stock = Stock.objects.get(nome = "negozio")
+    super().setUp()
+
+  def test_aggiungi_nuovo_prodotto(self):
+    count = self.stock.prodotti.count()
+    self.stock.aggiungi_nuovo_prodotto(
+      "Lavatrice",
+      "Elettrodomestici",
+      "Una bellissima lavatrice",
+      200,
+      100
     )
+    self.assertEqual(self.stock.prodotti.count(), count + 1)
+    self.assertEqual(self.stock.prodotti.get(id = count + 1).nome, "Lavatrice")
+
+  def test_rimuovi_prodotto(self):
+    count = self.stock.prodotti.count()
+    self.assertGreater(count, 0)
+    self.stock.rimuovi_prodotto(self.stock.prodotti.get(nome = "Motorino"))
+    with self.assertRaises(Prodotto.DoesNotExist):
+      self.stock.prodotti.get(nome = "Motorino")
+    self.assertEqual(self.stock.prodotti.count(), count - 1)
+
+class CarrelloTestCase(MasterTestCase):
+
+  def setUp(self):
+    super().setUp()
+
+  def test_aggiungi_prodotto(self):
+    count = self.carrello.prodotti.count()
+    prodotto = Stock.objects.get(nome = "Negozio").prodotti.get(nome = "Motorino")
+    self.carrello.aggiungi_prodotto(prodotto)
+    self.assertEqual(self.carrello.prodotti.count(), count + 1)
+    self.assertIsNotNone(self.carrello.prodotti.get(carrello = self.carrello, prodotto = prodotto))
+
+  def test_rimuovi_prodotto(self):
+    prodotto = Stock.objects.get(nome = "Negozio").prodotti.get(nome = "Motorino")
+    quantita = 10
+    self.carrello.aggiungi_prodotto(prodotto, quantita)
+    self.carrello.rimuovi_prodotto(prodotto)
+    self.assertEqual(self.carrello.prodotti.get(prodotto = prodotto).quantita, quantita - 1)
+
+  def test_totale(self):
+    prodotto = Stock.objects.get(nome = "Negozio").prodotti.get(nome = "Motorino")
+    quantita = 2
+    self.carrello.aggiungi_prodotto(prodotto, quantita)
+    totale_atteso = quantita * prodotto.prezzo
+    prodotto = self.stock.aggiungi_nuovo_prodotto(
+      "Lavatrice",
+      "Elettrodomestici",
+      "Una bellissima lavatrice",
+      200,
+      100
+    )
+    quantita = 5
+    self.carrello.aggiungi_prodotto(prodotto, quantita)
+    totale_atteso += quantita * prodotto.prezzo
+    self.assertEqual(self.carrello.totale(), totale_atteso)
+
+#================================================
+# Prodotto
+#================================================
+
+class ProdottoTestCase(MasterTestCase):
+
+  def setUp(self):
+    super().setUp()
+
   def test_modifica(self):
-    nuovo_nome = "Smart TV+"
-    nuovo_prezzo = 1749.99
-    self.prodotto.modifica(prezzo = nuovo_prezzo, nome = nuovo_nome)
-    self.assertEqual(self.prodotto.nome, nuovo_nome)
-    self.assertEqual(self.prodotto.prezzo, nuovo_prezzo)
+    prodotto = self.stock.prodotti.get(nome = "Motorino")
+    nome = "Monopattino elettrico"
+    descrizione = "Un monopattino ecologico con impatto zero sull'ambiente."
+    prodotto.modifica(descrizione = descrizione, nome = nome)
+    self.assertEqual(prodotto.nome, nome)
+    self.assertEqual(prodotto.descrizione, descrizione)
+
+class ProdottoCarrelloTestCase(MasterTestCase):
+
+  def setUp(self):
+    super().setUp()
+
+  def test_modifica_quantita(self):
+    prodotto = self.stock.prodotti.get(nome = "Motorino")
+    self.carrello.aggiungi_prodotto(prodotto, 5)
+    count = self.carrello.prodotti.count()
+    prodotto_carrello = self.carrello.prodotti.get(prodotto = prodotto)
+    prodotto_carrello.modifica_quantita(+3)
+    self.assertEqual(prodotto_carrello.quantita, 8)
+    prodotto_carrello.modifica_quantita(-5)
+    self.assertEqual(prodotto_carrello.quantita, 3)
+    prodotto_carrello.modifica_quantita(-100)
+    self.assertEqual(self.carrello.prodotti.count(), count - 1)
+
+  def test_acquista(self):
+    prodotto = self.stock.prodotti.get(nome = "Motorino")
+    quantita_magazzino = prodotto.quantita
+    quantita_carrello = quantita_magazzino - 5 # 20
+    prodotto_carrello = self.carrello.aggiungi_prodotto(prodotto, quantita_carrello)
+    prodotto_carrello.acquista()
+    prodotto_venduto = ProdottoVenduto.objects.get(stock = self.stock, nome = "Motorino")
+    self.assertEqual(prodotto_venduto.quantita, quantita_carrello)
+    self.assertEqual(prodotto.quantita, quantita_magazzino - quantita_carrello)
+
+class ProdottoVendutoTestCase(MasterTestCase):
+  pass
