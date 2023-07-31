b5cbca594b65cbd3a607389e4dfcc6adf168b1c1
Chiara <chiara.ganga98@gmail.com>
2023-07-31 16:52:56 +0200

    viste e urls

--- negozio/applicazione/urls.py
+++ negozio/applicazione/urls.py
@@ -5,4 +5,12 @@ from . import views
 urlpatterns = [
   path("", views.login, name="login"),
   path("login/", views.login, name="login"),
+  path("registrazione/", views.registrazione, name="registrazione"),
+  path("home/", views.home, name="home"),
+  path("checkout/", views.checkout, name="checkout"),
+  path("carrello/", views.carrello, name="carrello"),
+  path("aggiungi_prodotto/", views.aggiungi_prodotto, name="aggiungi_prodotto"),
+  path("home_amministratore/", views.home_amministratore, name="home_amministrazione"),
+  path("resoconto_vendite/", views.resoconto_vendite, name="resoconto_vendite"),
+  
 ]
--- negozio/applicazione/views.py
+++ negozio/applicazione/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from applicazione.models import *
 
 def login(request):
   template = loader.get_template("login.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+#Devo fare una funzione per ogni pagina request è il PAYLOAD, context passo i valori alla pagina
+
+#Form con Python(?)
+def registrazione(request):
+  template = loader.get_template("utente/registrazione.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+#Bisogna popolare la lista degli oggetti, stock prendo negozio nel contesto cerco i prodotti che si trovano lì
+def home(request):
+  template = loader.get_template("utente/home.html")
+  stock = Stock.objects.get(nome="Negozio")
+  context = {"prodotti" : Prodotto.objects.get(stock = stock)}
+  return HttpResponse(template.render(context, request))
+
+def checkout(request):
+  template = loader.get_template("utente/checkout.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def carrello(request):
+  template = loader.get_template("utente/carrello.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def aggiungi_prodotto(request):
+  template = loader.get_template("amministratore/Aggiungi_prodotto.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def home_amministratore(request):
+  template = loader.get_template("amministratore/Home_amministratore.html")
+  context = {}
+  return HttpResponse(template.render(context, request))
+
+def resoconto_vendite(request):
+  template = loader.get_template("amministratore/Resoconto_vendite.html")
   context = {}
   return HttpResponse(template.render(context, request))
