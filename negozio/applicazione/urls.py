f291fab826ad5c04d41ace685c1b5b110c8d762f
Chiara <chiara.ganga98@gmail.com>
2023-08-01 16:41:16 +0200

    modifiche viste e correzioni html

--- negozio/applicazione/urls.py
+++ negozio/applicazione/urls.py
@@ -5,12 +5,13 @@ from . import views
 urlpatterns = [
   path("", views.login_view, name="login_view"),
   path("login/", views.login_view, name="login_view"),
-  path("registrazione/", views.registrazione, name="registrazione"),
-  path("home/", views.home, name="home"),
-  path("checkout/", views.checkout, name="checkout"),
+  path("login_result/", views.login_form_result, name="login_form_result"),
+  path("registrazione/", views.registrazione_view, name="registrazione_view"),
+  path("home/", views.home_view, name="home_view"),
+  path("checkout/", views.checkout_view, name="checkout_view"),
   path("carrello/", views.carrello_view, name="carrello_view"),
-  path("aggiungi_prodotto/", views.aggiungi_prodotto, name="aggiungi_prodotto"),
-  path("home_amministratore/", views.home_amministratore, name="home_amministrazione"),
-  path("resoconto_vendite/", views.resoconto_vendite, name="resoconto_vendite"),
+  path("aggiungi_prodotto/", views.aggiungi_prodotto_view, name="aggiungi_prodotto_view"),
+  path("home_amministratore/", views.home_amministratore_view, name="home_amministratore_view"),
+  path("resoconto_vendite/", views.resoconto_vendite_view, name="resoconto_vendite_view"),
   
 ]
--- negozio/applicazione/views.py
+++ negozio/applicazione/views.py
@@ -2,24 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.auth import authenticate, login
+from applicazione.forms import *
 from applicazione.models import *
 
 def login_view(request):
   login(request, User.objects.get(username = "cliente"))
+  form = LoginForm()
   template = loader.get_template("login.html")
-  context = {}
+  context = {"form" : form}
   return HttpResponse(template.render(context, request))
+
+def login_form_result(request):
+  if request.method =='POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password']
+      return home_view(request)
 
 #Devo fare una funzione per ogni pagina request è il PAYLOAD, context passo i valori alla pagina
 
 #Form con Python(?)
-def registrazione(request):
+def registrazione_view(request):
   template = loader.get_template("utente/registrazione.html")
   context = {}
   return HttpResponse(template.render(context, request))
 
 #Bisogna popolare la lista degli oggetti, stock prendo negozio nel contesto cerco i prodotti che si trovano lì
-def home(request):
+def home_view(request):
   template = loader.get_template("utente/home.html")
   stock = Stock.objects.get(nome="Negozio")
   context = {"prodotti" : Prodotto.objects.filter(stock = stock).all()}
@@ -27,7 +37,7 @@ def home(request):
 
 #get restituisce un solo oggetto, filter tutti quelli che soddisfano la condizione
 
-def checkout(request):
+def checkout_view(request):
   template = loader.get_template("utente/checkout.html")
   context = {}
   return HttpResponse(template.render(context, request))
@@ -40,19 +50,19 @@ def carrello_view(request):
   context = {"prodottiCarrello" : request.user.carrello.prodotti.all(), "carrello" : carrello}
   return HttpResponse(template.render(context, request))
 
-def aggiungi_prodotto(request):
+def aggiungi_prodotto_view(request):
   template = loader.get_template("amministratore/Aggiungi_prodotto.html")
   context = {}
   return HttpResponse(template.render(context, request))
 
 #"prodottiNegozio"
-def home_amministratore(request):
+def home_amministratore_view(request):
   template = loader.get_template("amministratore/Home_amministratore.html")
   context = {}
   return HttpResponse(template.render(context, request))
 
 #"resocontoVendite"
-def resoconto_vendite(request):
+def resoconto_vendite_view(request):
   template = loader.get_template("amministratore/Resoconto_vendite.html")
   context = {}
   return HttpResponse(template.render(context, request))
