64f642ff915fe47ca72d815474e658424d7e7313
Manolo Sainas <sainas.manolo@gmail.com>
2023-08-02 13:34:28 +0200

    Modifica del file test accettazione (non funziona)

--- negozio/tests/accettazione/tests.py
+++ negozio/tests/accettazione/tests.py
@@ -2,15 +2,16 @@ from django.test import TestCase
 
 from selenium import webdriver
 
-from selenium.webdriver.common.keys
-
-import Keys 
+from selenium.webdriver.common.keys import Keys 
 import time 
 import os
 
 #test prova
 
+driver = webdriver.Chrome("C:\chromedriver.exe") 
+driver.get("https://www.google.com") 
+time.sleep(2) 
 ricerca=driver.find_element("id","APjFqb") 
 ricerca.send_keys("Ciao") 
 ricerca.send_keys(Keys.ENTER)
-Time.sleep(10)
+time.sleep(10)
