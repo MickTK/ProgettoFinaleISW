1b827c0260378d4b155dc5c63664dd40803c42a1
Manolo Sainas <sainas.manolo@gmail.com>
2023-08-02 13:13:27 +0200

    test accettazione - prova test base

--- negozio/tests/accettazione/tests.py
+++ negozio/tests/accettazione/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from selenium import webdriver
+
+from selenium.webdriver.common.keys
+
+import Keys 
+import time 
+import os
+
+#test prova
+
+ricerca=driver.find_element("id","APjFqb") 
+ricerca.send_keys("Ciao") 
+ricerca.send_keys(Keys.ENTER)
+Time.sleep(10)
