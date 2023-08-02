99c7ee5d26fc3a1a6ebcab546f9f6165d1b013c4
Michele <63038410+MickTK@users.noreply.github.com>
2023-08-02 19:00:50 +0200

    Viste

--- negozio/applicazione/views.py
+++ negozio/applicazione/views.py
@@ -1,31 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.contrib.auth import authenticate, login
 from applicazione.forms import *
 from applicazione.models import *
 
-def login_view(request):
-  form = LoginForm()
-  template = loader.get_template("login.html")
-  context = {"form" : form}
+NOME_STOCK = "Negozio"
 
-  if request.method =='POST':
+def login_view(request):
+  # Inizializzazione
+  request.session["user_id"] = None
+  template = loader.get_template("login.html")
+  context = {"form" : LoginForm()}
+
+  # Gestione richieste
+  if request.method =="POST":
     form = LoginForm(request.POST)
     if form.is_valid():
-      username = form.cleaned_data['username']
-      password = form.cleaned_data['password']
-      user = authenticate(request, username=username, password=password)
-
-      if user is None : 
-        return HttpResponse(template.render(context, request))
-      else:
+      user = authenticate(request,
+        username = form.cleaned_data["username"],
+        password = form.cleaned_data["password"]
+      )
+      if user is not None : 
         login(request, user)
-        return home_view(request)
+        request.session["user_id"] = request.user.id
+        return redirect("../home/")
+
+  return HttpResponse(template.render(context, request))
+
+def home_view(request):
+  # Inizializzazione
+  user_id = request.session.get("user_id")
+  if user_id:
+    user = User.objects.get(id=user_id)
+  else:
+   return redirect("../login/")
+  
+  template = loader.get_template("utente/home.html")
+  stock = Stock.objects.get(nome = NOME_STOCK)
+  context = {"prodotti" : Prodotto.objects.filter(stock = stock).all()}
   return HttpResponse(template.render(context, request))
 
 
-#Devo fare una funzione per ogni pagina request è il PAYLOAD, context passo i valori alla pagina
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def registrazione_view(request):
@@ -33,12 +63,7 @@ def registrazione_view(request):
   context = {}
   return HttpResponse(template.render(context, request))
 
-#Bisogna popolare la lista degli oggetti, stock prendo negozio nel contesto cerco i prodotti che si trovano lì
-def home_view(request):
-  template = loader.get_template("utente/home.html")
-  stock = Stock.objects.get(nome="Negozio")
-  context = {"prodotti" : Prodotto.objects.filter(stock = stock).all()}
-  return HttpResponse(template.render(context, request))
+
 
 #get restituisce un solo oggetto, filter tutti quelli che soddisfano la condizione
 
@@ -71,7 +96,7 @@ def checkout_view(request):
     return HttpResponse(template.render(context, request))
 
 
-#Come visualizzo il carrello dell'utente? "prodottiCarrello" Request.user
+#Come visualizzo il carrello dell"utente? "prodottiCarrello" Request.user
 #def carrello_view(request):
  # login(request, User.objects.get(username = "cliente"))
  # template = loader.get_template("utente/carrello.html")
