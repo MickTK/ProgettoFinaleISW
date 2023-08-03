fcbba0d8452f854396952ca005c6a9e756a9e071
Manolo Sainas <sainas.manolo@gmail.com>
2023-08-03 16:23:43 +0200

    Creazione Vista (home_amministratore_view)

--- negozio/applicazione/views.py
+++ negozio/applicazione/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth import authenticate, login, logout
@@ -193,8 +193,68 @@ def aggiungi_prodotto_view(request):
 #"prodottiNegozio"
 def home_amministratore_view(request):
   template = loader.get_template("amministratore/Home_amministratore.html")
-  context = {}
+  stock = Stock.objects.get(nome=NOME_STOCK)  
+  context = {
+    "prodotti" : Prodotto.objects.filter(stock=stock).all(),
+    "form" : FiltroHomeAmministratoreForm()
+  }
+
+
+  if request.method == "POST":
+    form = FiltroHomeAmministratoreForm(request.POST)  # Form per gestire i filtri
+
+    if form.is_valid():
+      nome = form.cleaned_data["nome"]
+      tipologia = form.cleaned_data["tipologia"]
+      minPrezzo = form.cleaned_data["minPrezzo"]
+      maxPrezzo = form.cleaned_data["maxPrezzo"]
+      min_prodotti = form.cleaned_data("minNumPezziDisponibili")
+      max_prodotti = form.cleaned_data("maxNumPezziDisponibili")
+      
+      nome_check = tipologia_check = prezzo_check = prodotti_disponibili_check = None
+
+      prodotti = context["prodotti"]
+      context["prodotti"] = list()
+
+      for prodotto in prodotti:
+        if nome is not None:
+          nome_check = prodotto.nome.lower().find(nome.lower()) > -1
+        if tipologia is not None:
+          tipologia_check = prodotto.tipologia.lower().find(tipologia.lower()) > -1
+          
+        if minPrezzo is not None:
+          prezzo_check = minPrezzo < prodotto.prezzo
+        if maxPrezzo is not None:
+          prezzo_check = prodotto.prezzo < maxPrezzo
+          
+        if minPrezzo is not None and maxPrezzo is not None:
+          prezzo_check = minPrezzo < prodotto.prezzo and prodotto.prezzo < maxPrezzo
+          
+        if min_prodotti is not None:
+          prodotti_disponibili_check = min_prodotti < prodotto.quantita
+        if max_prodotti is not None:
+          prodotti_disponibili_check = prodotto.quantita < max_prodotti
+          
+        if min_prodotti is not None and max_prodotti is not None:
+          prodotti_disponibili_check = min_prodotti < prodotto.quantita and prodotto.prezzo < max_prodotti
+
+        if (nome_check or nome_check is None) and (tipologia_check or tipologia_check is None) and (prezzo_check or prezzo_check is None) and (prodotti_disponibili_check or prodotti_disponibili_check is None):
+          context["prodotti"].append(prodotto)
+
+      valori_iniziali_form = {
+        "nome": nome,
+        "tipologia": tipologia,
+        "minPrezzo": minPrezzo,
+        "maxPrezzo": maxPrezzo,
+        "minNumPezziDisponibili": min_prodotti,
+        "maxNumPezziDisponibili": max_prodotti
+      }
+      context["form"] = FiltroHomeAmministratoreForm(initial = valori_iniziali_form)
+
   return HttpResponse(template.render(context, request))
+
+  
+  
 
 #"resocontoVendite"
 def resoconto_vendite_view(request):
