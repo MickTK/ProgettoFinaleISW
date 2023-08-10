5d00631a11dc8f5075b57310e38629dd241c92c3
Chiara <chiara.ganga98@gmail.com>
2023-08-10 20:36:25 +0200

    Test Accettazione
    modifica unico driver per i test di accettazione
    
    Co-Authored-By: Manolo Sainas <109757444+ManoloSainas@users.noreply.github.com>
    Co-Authored-By: Michele <63038410+MickTK@users.noreply.github.com>

--- negozio/tests/accettazione/test/test_login.py
+++ negozio/tests/accettazione/test/test_login.py
@@ -6,19 +6,10 @@ from selenium import webdriver
 
 from pagine.login_page import Login
 
-class TestLogin(unittest.TestCase):
     # inizializza il driver e l'istanza della classe login
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.login_page = Login(self.driver)
 
     # test del login con dati corretti
-    def test_successful_login(self):
-        self.login_page.login("cliente", "cliente")
-        # assert per verificare il successo del login
-        expected_url = "http://127.0.0.1:8000/home/"
-        actual_url = self.driver.current_url
-        self.assertEqual(expected_url, actual_url)  
+    
 
     # test del login con dati errati
     def test_failed_login(self):
--- negozio/tests/accettazione/test_accettazione.py
+++ negozio/tests/accettazione/test_accettazione.py
@@ -2,31 +2,63 @@
 import unittest
 
 from django.test import TestCase
+import time
 
 # framework
 from selenium import webdriver
 
+'''
 from test.test_login import TestLogin
 from test.test_registrazione import TestRegistrazione
 from test.test_logout import TestLogout
-from test.test_aggiungi_prodotto_al_carrello import TestAggiungiProdottoAlCarrello
+from test.test_aggiungi_prodotto_al_carrello import TestAggiungiProdottoAlCarrello '''
 
-if __name__ == '__main__':
+from tests.accettazione.pagine.login_page import Login
+
+
+class AccettazioneTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Inizializza il browser   
+        cls.driver = webdriver.Chrome()
+
+    @classmethod
+    def tearDownClass(cls):
+        # Chiudi il browser
+        cls.driver.quit()
+    
+
+    def setUp(self):
+        pass
+
+    def test_successful_login(self):
+        login_page = Login(self.driver)
+        login_page.login("cliente", "cliente")
+        # assert per verificare il successo del login
+        expected_url = "http://127.0.0.1:8000/home/"
+        actual_url = self.driver.current_url
+        self.assertEqual(expected_url, actual_url)  
+        time.sleep(10)
+
+    def tearDown(self):
+        pass 
+
+'''if __name__ == '__main__':
     # Inizializza il browser
-    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome()
 
     # Inizializza il runner dei test
     test_runner = unittest.TextTestRunner()
 
     # Esegue i test
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestAggiungiProdottoAlCarrello))
-    # suite.addTest(unittest.makeSuite(TestLogout))
-    # suite.addTest(unittest.makeSuite(TestRegistrazione))
-    # suite.addTest(unittest.makeSuite(TestLogin))
+    #suite.addTest(unittest.makeSuite(TestAggiungiProdottoAlCarrello))
+    #suite.addTest(unittest.makeSuite(TestLogout))
+    #suite.addTest(unittest.makeSuite(TestRegistrazione))
+    suite.addTest(unittest.makeSuite(TestLogin))
     
 
     result = test_runner.run(suite)
 
     # Chiudi il browser
-    driver.quit()
+    driver.quit() '''
